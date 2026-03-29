98e16c0bb806d8fedd5c88d382801b87117b12f6
joanysavignon <joanysavignon@gmail.com>
2026-03-29 18:39:12 -0400

    Add files via upload

---
+++ Expense_Tracker/supabase-config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: "https://itntgnunuakqqdukprta.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0bnRnbnVudWFrcXFkdWtwcnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MjE3MjIsImV4cCI6MjA5MDM5NzcyMn0.Fp8XYG6wtcO_cv3ndb8Kb7VbWdPa3uCf4IwF_Aja1mw",
+};
